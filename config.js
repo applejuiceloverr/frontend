0aa96d2658986e6cd64cbc3168e2d470daa73e75
applejuiceloverr <155693252+applejuiceloverr@users.noreply.github.com>
2025-02-18 01:52:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    apiUrl: process.env.API_URL || 'http://localhost:8000', // Default to localhost if not set
+    apiUrl: process.env.API_URL || 'https://flowers-app-backend-latest.onrender.com', // Default to localhost if not set
   };
   
   export default config;
